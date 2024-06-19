67ee1b3242baf49509ae19e7b6b637e49144c60f
Konstantin <k.lisovskiy@icloud.com>
2024-06-19 19:16:09 +0200

    Initial Commit

---
+++ AgeCalculator/AgeCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AgeCalculatorApp.swift
+//  AgeCalculator
+//
+//  Created by Konstantin Lisovskiy on 19/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct AgeCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
